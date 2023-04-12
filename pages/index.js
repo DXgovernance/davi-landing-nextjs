7a359b8c0fd7551756c934548327f21bd0691ee4
rossneilson <rossneilson99@gmail.com>
2023-04-12 15:29:57 +0900

    fix: Remove log

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ export default function Home() {
   const [email, setEmail] = useState();
   const { t } = useTranslation();
   const { lang } = useSelectedLanguage();
-  console.log({ lang });
 
   return (
     <>
